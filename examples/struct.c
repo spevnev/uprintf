b676dd5cb5efcca5231aba5ca43c0fceeb982b2f
spevnev <spevnev16@gmail.com>
2024-06-10 18:00:32 +0300

    Add struct example

--- examples/struct.c
+++ examples/struct.c
@@ -1,4 +1,4 @@
-// Printing a structure containing all C-types and typedefs.
+// Printing structs defined: locally, globally, as parameter, as pointer to parameter, as void*
 
 #define UPRINTF_IMPLEMENTATION
 #include "uprintf.h"
@@ -29,10 +29,46 @@ typedef struct {
     int *int_ptr;
     float *null_ptr;
     char *ch_ptr;
-    cstr str;
+
+    const char *str;
     const cstr const_str;
 } MyStruct;
 
+
+static int i = 5;
+static char c = 'c';
+
+static MyStruct gs = {
+    .u8 = 255,
+    .u16 = 65535,
+    .u32 = 4294967295,
+    .u64 = 18446744073709551615UL,
+
+    .i8 = -128,
+    .i16 = -32768,
+    .i32 = -2147483648,
+    .i64 = -9223372036854775807L,
+
+    .f32 = 0.123,
+    .f64 = -0.321,
+
+    .uch = 'c',
+    .sch = -'c',
+
+    .void_ptr = &i,
+    .int_ptr = &i,
+    .null_ptr = NULL,
+    .ch_ptr = &c,
+    .str = "const char *str",
+    .const_str = "const_str",
+};
+
+
+void print_struct(MyStruct s) { uprintf("parameter `s` contains %S\n", &s); }
+
+void print_struct_ptr(MyStruct *s) { uprintf("parameter `*s` contains %S\n", s); }
+
+void print_void(void *v) { uprintf("parameter `v` contains %S\n", (MyStruct *) v); }
 
 int main(void) {
     int i = 5;
@@ -65,14 +101,10 @@ int main(void) {
     };
 
     uprintf("variable `s` contains %S\n", &s);
-
-    // variable: local, parameter, global
-    // pointer: struct, union, primitive
-    // argument: variable, referencing variable, casting variable
-    //! don't work? try to at least show error: inline call
-    // test: a lot of variables (i vaguely remember seeing different DWARF TAG)
-    // modifiers: pointer, const value, const pointer, volatile, register, restricted, static
-
+    uprintf("variable `gs` contains %S\n", &gs);
+    print_struct(s);
+    print_struct_ptr(&s);
+    print_void(&s);
 
     return 0;
 }
