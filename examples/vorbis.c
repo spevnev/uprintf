9b7101c6b608a6449a613cd07023cd34d5dfea75
spevnev <spevnev16@gmail.com>
2024-08-09 10:43:43 -0400

    Add std_vorbis example

---
+++ examples/vorbis.c
@@ -0,0 +1,22 @@
+#include "stb_vorbis.c"
+#define UPRINTF_IMPLEMENTATION
+#include "uprintf.h"
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        fprintf(stderr, "usage: %s [path-to-ogg]\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    int error = 0;
+    stb_vorbis *vorbis = stb_vorbis_open_filename(argv[1], &error, NULL);
+    if (vorbis == NULL) {
+        fprintf(stderr, "Vorbis error: %d\n", error);
+        return EXIT_FAILURE;
+    }
+
+    uprintf("Vorbis data: %S\n", vorbis);
+
+    stb_vorbis_close(vorbis);
+    return EXIT_SUCCESS;
+}
