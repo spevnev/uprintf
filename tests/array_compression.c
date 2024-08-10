448d90fbbff29c34a7cfd04b9bbf1d367d644f2b
spevnev <spevnev16@gmail.com>
2024-08-09 20:23:25 -0400

    Add array_compression test

---
+++ tests/array_compression.c
@@ -0,0 +1,14 @@
+#define UPRINTF_IMPLEMENTATION
+#include "uprintf.h"
+
+int main(void) {
+    uint8_t array[32];
+    memset(array, 1, 32);
+    uprintf("Array of 1s: %S\n", &array);
+
+    uint8_t arrays[16][8];
+    memset(arrays, 2, 16 * 8);
+    uprintf("Array of arrays of 2s: %S\n", &arrays);
+
+    return _upf_test_status;
+}
