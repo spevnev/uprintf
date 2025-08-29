257ed7d899f435446ffcfba12e2ac85361d1877b
spevnev <spevnev16@gmail.com>
2025-08-29 17:42:59 -0400

    Rename arg to grouping

--- tests/grouping.c
+++ tests/grouping.c
@@ -3,11 +3,7 @@
 int main(void) {
     char c = 'c';
     int i = 1;
-
     uprintf("int %S\n", ((void) c, &i));
     uprintf("int %S\n", ((void) ((void) c, i), &i));
-    uprintf("int %S\n", ((void) "string with ,", &i));
-    uprintf("int %S\n", ((void) "string with \" and ,", &i));
-
     return _upf_test_status;
 }
